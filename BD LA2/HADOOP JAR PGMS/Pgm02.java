0e3066d438c5908c25c95003230a8e82284a2d37
1NT18IS201 <1nt18is201.karthik@nmit.ac.in>
2021-06-09 15:58:53 +0530

    LA2 repo for report
    All files required to be linked in the la2 report for BD lab

---
+++ BD LA2/HADOOP JAR PGMS/Pgm02.java
@@ -0,0 +1,63 @@
+package my.pack.emp;
+
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+
+
+
+public class Pgm02 {
+
+	//MAPPER CODE	
+		   
+	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+	
+	// the variable one holds the value 1 in the form of Hadoop's IntWritable Object
+	private final static IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+		String myvalue = value.toString();
+		String[] tokens = myvalue.split(",");
+		int token_int = Integer.parseInt(tokens[3]);
+		IntWritable awardcnt = new IntWritable(token_int);
+		output.collect(new Text("Award"), awardcnt);
+		
+		}
+	}
+
+	//REDUCER CODE	
+	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException { 
+			int count = 0;
+			while(values.hasNext()) {
+				count += values.next().get();
+			}
+			
+			output.collect(new Text("No. of awards won by employees: "), new IntWritable(count));
+		}
+	}
+		
+	
+	//DRIVER CODE
+	public static void main(String[] args) throws Exception {
+		JobConf conf = new JobConf(Pgm02.class);
+		conf.setJobName("Awards Counting");
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(IntWritable.class);
+		conf.setMapperClass(Map.class);
+		conf.setCombinerClass(Reduce.class);
+		conf.setReducerClass(Reduce.class);
+		conf.setInputFormat(TextInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class); // hadoop jar jarname classpath inputfolder outputfolder
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		JobClient.runJob(conf);   
+	}
+	}
